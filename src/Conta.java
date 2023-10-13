df5108c9ef4ef2dbb73636573e22755cdff712b7
Carollinaa <carolina.ant.16@gmail.com>
2023-10-12 22:50:34 -0300

    melhoria no histórico

--- src/Conta.java
+++ src/Conta.java
@@ -15,7 +15,7 @@ class Conta {
     public String depositar(double valor){
         if(valor > 0){
             this.saldo = saldo + valor;
-            this.historico.add("Deposito de "+ valor);
+            this.historico.add("Deposito realizado de "+ valor + " reais na conta " + this.numConta);
             return "Deposito bem sucedido!";
         }else{
             return "O valor do depósito não pode ser igual a 0 e não pode ser negativo";
@@ -24,7 +24,7 @@ class Conta {
     public String sacar(double valor){
         if(this.saldo >= valor){
             this.saldo = saldo - valor;
-            this.historico.add("Saque de "+ valor);
+            this.historico.add("Saque realizado de "+ valor + " reais na conta " + this.numConta);
             return "Saque bem sucedido!";
         }else{
             return "Saldo insuficiente para realizar o saque!";
@@ -35,16 +35,21 @@ class Conta {
         if(this.saldo >= valor){
             this.saldo -= valor;
             conta.setSaldo(valor);
-            this.historico.add("Transferência de "+ valor);
+            this.historico.add("Transferência realizada de "+ valor + " reais da conta " + this.numConta + " para a conta " + conta.getNunConta());
             return "Tranferencia bem sucedida!";
         }else{
             return "Saldo insuficiente para realizar a transferencia!";
         }
     } 
+    
     public void exibirExtrato(){
         for(int i = 0; i < historico.size(); i++){
             System.out.println(historico.get(i));
         }
+    }
+
+    private int getNunConta() {
+        return this.numConta;
     }
 
     public double getSaldo() {
